ca7a8f660db2f9f67e9a596661e2a148dd8c0e5e
Stella <syampolsky50@stuy.edu>
2025-02-05 11:23:03 -0500

    testing shit

---
+++ add_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter filename: " filename
+touch "$filename"
+read -p "Enter text: " txt
+echo "$txt" > "$filename"
+git add .
+read -p "Enter commit message: " commit
+git commit -m "$commit"
+git push
+
