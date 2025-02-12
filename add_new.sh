8d83d3520e1adbc52c617cc47c2d457a0ba4d33f
Stella Yampolsky <144484124+Stella-Yampolsky@users.noreply.github.com>
2025-02-12 10:28:40 -0500

    Update add_new.sh

--- add_new.sh
+++ add_new.sh
@@ -2,7 +2,7 @@
 read -p "Enter filename: " filename
 touch "$filename"
 read -p "Enter text: " txt
-echo "$txt" > "$filename"
+echo "$txt" > "$filename "
 git add "$filename"
 read -p "Enter commit message: " commit
 git commit -m "$commit"
