3d9ec0a213a2e1e75c6346e82f009adda02821b2
Stella Yampolsky <144484124+Stella-Yampolsky@users.noreply.github.com>
2025-02-12 10:27:52 -0500

    better commit message

--- add_all.sh
+++ add_all.sh
@@ -2,9 +2,9 @@
 read -p "Enter filename: " filename
 touch "$filename"
 read -p "Enter text: " txt
-echo "$txt" > "$filename"
+echo "$txt" > "$filename:"
 git add .
 read -p "Enter commit message: " commit
-git commit -m "$commit"
+git commit -m "$commit:"
 git push
 
